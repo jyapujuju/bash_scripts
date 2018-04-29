a6e2984624e8c825619fba2f48194c9865d47849
Rabin Shrestha <danieldangol@gmail.com>
2018-04-29 11:09:12 +0545

    cmdtest is conflicting with yarn
    remove the cmdtest

--- rails_simple.sh
+++ rails_simple.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt remove cmdtest
 sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev nodejs yarn
 cd $HOME
@@ -12,6 +16,8 @@ echo "after shell"
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 eval $(echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc)
 . ~/.bashrc
+
+exec $SHELL
 
 rbenv install 2.5.0
 rbenv global 2.5.0
